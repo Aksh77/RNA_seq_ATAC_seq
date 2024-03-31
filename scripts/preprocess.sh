e44227d87219bbbd28827f1fafdcf967b48760a7
Akshatha Nayak <akshanayaki@gmail.com>
2024-03-31 17:07:30 -0400

    get consensus peaks

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -5,21 +5,40 @@ for bigbed in $(ls data/input_data/*/*.bigBed); do
 	bed=$(echo $bigbed | sed 's/.bigBed/.bed/')
 	utils/bigBedToBed $bigbed $bed
 
-	# convert bed to bedgraph
-	bedgraph=$(echo $bed | sed 's/.bed/.bedgraph/')
-	cut -f1,2,3,4 $bed | sort -k1,1 -k2,2n > $bedgraph
+	# merge overlapping regions in bed
+	bed_merged=$(echo $bed | sed 's/.bed/.merged.bed/')
+	cat $bed | awk -F '\t' -v OFS='\t' '{print $1,$2,$3-1,$4}' > data/bed.tmp
+	bedtools merge -i data/bed.tmp -c 4 -o mean > data/bed.merged.tmp
+	cat data/bed.merged.tmp | awk -F '\t' -v OFS='\t' '{print $1,$2,$3+1,$4}' > $bed_merged
 
-	# merge overlapping regions in bedgraph
-	bedgraph_merged=$(echo $bedgraph | sed 's/.bedgraph/.merged.bedgraph/')
-	cat $bedgraph | awk -F '\t' -v OFS='\t' '{print $1,$2,$3-1,$4}' > bedGraph.tmp
-	bedtools merge -i bedGraph.tmp -c 4 -o mean > bedGraph.merged.tmp
-	cat bedGraph.merged.tmp | awk -F '\t' -v OFS='\t' '{print $1,$2,$3+1,$4}' > $bedgraph_merged
-	
 	# remove temp files
-	rm bedGraph.tmp bedGraph.merged.tmp
+	rm data/bed.tmp data/bed.merged.tmp
+
+	# convert bed to bedgraph
+	bedgraph=$(echo $bed_merged | sed 's/.merged.bed/.bedgraph/')
+	cut -f1,2,3,4 $bed_merged | sort -k1,1 -k2,2n > $bedgraph
 
 	# convert bedgraph to bigwig
-	bigwig=$(echo $bedgraph_merged | sed 's/.merged.bedgraph/.bigWig/')
-	utils/bedGraphToBigWig $bedgraph_merged utils/mm10.chrom.sizes.txt $bigwig
+	bigwig=$(echo $bedgraph | sed 's/.bedgraph/.bigWig/')
+	utils/bedGraphToBigWig $bedgraph utils/mm10.chrom.sizes.txt $bigwig
+
+done
+
+# get consensus peaks from replicates for each cell line
+for dir in $(ls -d data/input_data/*); do
+	# get cell line name from subdirectory name
+	seq=$(echo $dir | sed 's/data\/input_data\///')
+	seq=$(echo $seq | sed 's/\///')
+	seq=$(echo $seq | cut -d'_' -f2)
+	echo $seq
+
+	# output directory
+	out_dir=data/consensus_peaks
+	mkdir -p $out_dir
+
+	# intersect bed files in the same subdirectory to get consensus peaks
+	bed1=$(ls $dir/*.bed | head -n 1)
+	bed2=$(ls $dir/*.bed | tail -n 1)
+	bedtools intersect -a $bed1 -b $bed2  -f 0.50 -r > $out_dir/${seq}_consensus_peaks.bed
 done
 
