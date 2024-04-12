7eae53c3567ef39eccf99779acd1f345aca97e3a
Akshatha Nayak <akshanayaki@gmail.com>
2024-04-12 16:26:05 -0400

    renaming subdirs

--- ATAC_seq/scripts/1_peak_calling.sh
+++ ATAC_seq/scripts/1_peak_calling.sh
@@ -1,7 +1,7 @@
 # peakcalling using MACS2
-for dir in $(ls -d data/input_data/*); do
+for dir in $(ls -d ATAC_seq/data/input_data/*); do
 	# get cell line name from subdirectory name
-	seq=$(echo $dir | sed 's/data\/input_data\///')
+	seq=$(echo $dir | sed 's/ATAC_seq/data\/input_data\///')
 	seq=$(echo $seq | sed 's/\///')
 	seq=$(echo $seq | cut -d'_' -f2)
 	echo $seq
@@ -9,7 +9,7 @@ for dir in $(ls -d data/input_data/*); do
 	# call peaks using MACS2
 	rep1=$(ls $dir/*.bam | head -n 1)
 	rep2=$(ls $dir/*.bam | tail -n 1)
-	peak_dir=data/peaks/$seq
+	peak_dir=ATAC_seq/data/peaks/$seq
 	mkdir -p $peak_dir
 	macs2 callpeak -t $rep1 -f BAM -g mm -n ${seq}_1 --outdir $peak_dir
 	macs2 callpeak -t $rep2 -f BAM -g mm -n ${seq}_2 --outdir $peak_dir
@@ -19,7 +19,7 @@ done
 SEQS="hsc cmp cfue erythroblast"
 for seq in $SEQS; do
 	seq=hsc
-	dir=data/peaks/${seq}
+	dir=ATAC_seq/data/peaks/${seq}
 	rep1=${dir}/${seq}_1_peaks.narrowPeak
 	rep2=${dir}/${seq}_2_peaks.narrowPeak
 	consensus=${dir}/${seq}_peaks.narrowPeak
