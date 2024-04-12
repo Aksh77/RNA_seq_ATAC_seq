1ddd0973361abaf07a901f40431e5f9efc30bb00
Akshatha Nayak <akshanayaki@gmail.com>
2024-04-12 18:46:00 -0400

    peak annotation and functional enrichment analysis for all contrasts

--- ATAC_seq/scripts/4_peak_annotation_and_functional_enrichment.R
+++ ATAC_seq/scripts/4_peak_annotation_and_functional_enrichment.R
@@ -11,8 +11,13 @@ library(org.Mm.eg.db)
 library(TxDb.Mmusculus.UCSC.mm10.knownGene)
 txdb <- TxDb.Mmusculus.UCSC.mm10.knownGene
 
-# Annotate peaks with closest genomic features
-# select only peaks in differentially accessible regions
+# read contrast from command line arguments
+# Usage: Rscript 4_peak_annotation_and_functional_enrichment.R hsc_cfue
+args = commandArgs(trailingOnly=TRUE)
+contrast = args[1]
+
+# Annotate peaks in in differentially accessible regions with closest genomic features
+DA.res.coords = read.table(paste("ATAC_seq/data/output_data/differential_accessibility/",contrast,"_differential_accessibility.tsv", sep=""), sep="\t", header=TRUE)
 peaks.gr = GRanges(seqnames=DA.res.coords$Chr, ranges=IRanges(DA.res.coords$Start, DA.res.coords$End), strand=DA.res.coords$Strand)
 bed.annot = annotatePeak(peaks.gr, tssRegion=c(-3000, 3000),TxDb=txdb, annoDb="org.Mm.eg.db")
 annot_peaks = as.data.frame(bed.annot)
@@ -23,7 +28,7 @@ if (file.exists(outdir) == FALSE) {
   dir.create(outdir,recursive=TRUE)
 }
 setwd(outdir)
-result_file = "hsc_vs_cfue_annotated_peaks.tsv"
+result_file = paste(contrast,"_annotated_peaks.tsv", sep="")
 write.table(annot_peaks, result_file,
             append = FALSE,
             quote = FALSE,
@@ -33,12 +38,14 @@ write.table(annot_peaks, result_file,
             fileEncoding = "")
 
 # Visualise the annotation summary
-pdf("hsc_vs_cfue_annotated_peaks.pdf")
+summary_plot = paste(contrast,"_annotated_peaks.pdf", sep="")
+pdf(summary_plot)
 upsetplot(bed.annot, vennpie=TRUE)
 dev.off()
 
 # Plot distribution of peaks relative to TSS
-pdf("hsc_vs_cfue_dist_to_tss.pdf")
+tss_dist_plot = paste(contrast,"_dist_to_tss.pdf", sep="")
+pdf(tss_dist_plot)
 plotDistToTSS(bed.annot, title="Distribution of ATAC-seq peaks loci\nrelative to TSS")
 dev.off()
 
@@ -55,7 +62,7 @@ setwd(outdir)
 if (file.exists(outdir) == FALSE) {
   dir.create(outdir,recursive=TRUE)
 }
-result_file = "hsc_vs_cfue_functional_enrichment.tsv"
+result_file = paste(contrast,"_enriched_pathways.tsv", sep="")
 write.table(pathway.reac, result_file,
             append = FALSE,
             quote = FALSE,
